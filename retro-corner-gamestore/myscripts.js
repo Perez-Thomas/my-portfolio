483d93ef023f591cf1fd1367391a7986cc29d556
Thomas Perez <thomas.v.perez@gmail.com>
2022-08-13 19:08:41 -0400

    updating retro corner repository

--- retro-corner-gamestore/myscripts.js
+++ retro-corner-gamestore/myscripts.js
@@ -1,21 +1,29 @@
-    
-    var i = 0;
-    var images = [];
-    var time = 1000;
+"use strict";
+var i = 0;
+var images = [];
+var time = 1000;
 
-    images[0]='image1.jpg';
-    images[1]='image2.jpg';
-    images[2]='image3.jpg';
+images[0] = "images/image1.jpg";
+images[1] = "images/image2.jpg";
+images[2] = "images/image3.jpg";
 
-    function changeImg(){
-        document.slide.src = images[i];
+function changeImg() {
+  document.hasChildNodes.src = images[i];
 
-        if (i < images.length - 1) {
-            i++;
-        } else {
-            i = 0;
-        }
-    }
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("changeImg()", time);
+}
+window.onload = changeImg;
 
-    setTimeout("changeImg()",time);
-    window.onload = changeImg;
+// onclick take to top of page
+
+// const submitButton = document.querySelector("#submit-form");
+// submitButton.addEventListener("click", topOfPage);
+
+// function topOfPage() {
+//   document.querySelector("html").style.backgroundColor = "lightblue";
+// }
